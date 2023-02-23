e3cba439d5c94542a1ae8b7b151791e57cf4c30f
ADBR1998 <49815396+ADBR1998@users.noreply.github.com>
2023-02-23 14:39:36 +0500

    Add files via upload

---
+++ 1-notebook.py
@@ -0,0 +1,196 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "d3ce60be",
+   "metadata": {},
+   "source": [
+    "# IBM Watson Studio AND Machine Learning"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "57a6bd8c",
+   "metadata": {},
+   "source": [
+    "### Welcome to my first Jupyter notebook on IBM Watson Studio, where I will explore machine learning tasks.\n",
+    "\n",
+    "\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "5b84434f",
+   "metadata": {},
+   "source": [
+    "#### Here are some popular data science languages:\n",
+    "\n",
+    "Python\n",
+    "R\n",
+    "SQL\n",
+    "MATLAB\n",
+    "Julia\n",
+    "Scala"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "98351458",
+   "metadata": {},
+   "source": [
+    "#### Here are some commonly used data science libraries:\n",
+    "\n",
+    "NumPy\n",
+    "Pandas\n",
+    "Matplotlib\n",
+    "Seaborn\n",
+    "Scikit-learn\n",
+    "TensorFlow\n",
+    "PyTorch\n",
+    "Keras"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "7de8ef5e",
+   "metadata": {},
+   "source": [
+    "#### Some popular tools for data science include Python libraries such as \n",
+    "NumPy, Pandas, Matplotlib, and Scikit-learn; R libraries such as ggplot2, dplyr, and caret; and software platforms such as IBM Watson Studio, Jupyter Notebook, and Apache Spark.\n",
+    "\n",
+    "\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "28516cfd",
+   "metadata": {},
+   "source": [
+    "#### Arithmetic expressions are mathematical statements that involve \n",
+    "numbers, operators, and sometimes variables. In this notebook, we will explore different examples of arithmetic expressions using various operators such as addition, subtraction, multiplication, and division.\n",
+    "\n",
+    "\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "5adea57f",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "The product of 5 and 3 is 15\n",
+      "The sum of 3 and 2 is 5\n"
+     ]
+    }
+   ],
+   "source": [
+    "# multiplying and adding numbers\n",
+    "num1 = 5\n",
+    "num2 = 3\n",
+    "num3 = 2\n",
+    "\n",
+    "# multiplying num1 and num2\n",
+    "result1 = num1 * num2\n",
+    "\n",
+    "# adding num2 and num3\n",
+    "result2 = num2 + num3\n",
+    "\n",
+    "# printing the results\n",
+    "print(\"The product of\", num1, \"and\", num2, \"is\", result1)\n",
+    "print(\"The sum of\", num2, \"and\", num3, \"is\", result2)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "e55e8d34",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "135 minutes is equal to 2 hours and 15 minutes\n"
+     ]
+    }
+   ],
+   "source": [
+    "# input number of minutes\n",
+    "minutes = 135\n",
+    "\n",
+    "# convert minutes to hours and minutes\n",
+    "hours = minutes // 60\n",
+    "remaining_minutes = minutes % 60\n",
+    "\n",
+    "# print result\n",
+    "print(f\"{minutes} minutes is equal to {hours} hours and {remaining_minutes} minutes\")\n"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "ac14bc29",
+   "metadata": {},
+   "source": [
+    "#### Objectives:\n",
+    "Familiarize with Jupyter Notebook and IBM Watson Studio.\n",
+    "Learn about Data Science Languages, Libraries and Tools.\n",
+    "Practice basic arithmetic expressions in Python.\n",
+    "Perform data transformation and analysis using Pandas library.\n",
+    "Create visualizations using Matplotlib library.\n",
+    "Introduction to Machine Learning with Scikit-learn library."
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "51585b4f",
+   "metadata": {},
+   "source": [
+    "### Author: Mohammad Ahmad Babar"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "f8cf7c12",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a8c0b8d4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
